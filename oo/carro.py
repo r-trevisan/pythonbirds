389719d3ec14a15eba1bdbb5cbfd6df5a1795af0
r-trevisan <rrhtt3@gmail.com>
2021-02-10 15:51:10 -0400

    Implementado o Carro

--- oo/carro.py
+++ oo/carro.py
@@ -75,27 +75,50 @@ os seguintes atributos:
     >>> carro = Carro(direcao, motor)
     >>> carro.calcular_velocidade()
     0
-    >>> carro.calcular_acelerar()
+    >>> carro.acelerar()
     >>> carro.calcular_velocidade()
     1
-    >>> carro.calcular_acelerar()
+    >>> carro.acelerar()
     >>> carro.calcular_velocidade()
     2
     >>> carro.frear()
     >>> carro.calcular_velocidade()
     0
-    >>> carro.calcular_direcao()
-    >>> 'Norte'
+    >>> carro.calcular_a_direcao()
+    'Norte'
     >>> carro.girar_a_direita()
-    >>> carro.calcular_direcao()
-    >>> 'Leste'
+    >>> carro.calcular_a_direcao()
+    'Leste'
     >>> carro.girar_a_esquerda()
     >>> carro.calcular_a_direcao()
-    >>> 'Norte'
+    'Norte'
     >>> carro.girar_a_esquerda()
     >>> carro.calcular_a_direcao()
-    >>> 'Oeste'
+    'Oeste'
 '''
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_a_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
 
 class Motor():
     def __init__(self):
